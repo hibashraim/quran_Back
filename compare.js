752f20fc40198020ef9721d2bbf72c1314bd08e6
hibashraim <hiba.shraim.25@gmail.com>
2024-04-06 08:14:41 +0300

    postHandler

--- compare.js
+++ compare.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import natural from 'natural';
 
-const tokenizer = new natural.WordTokenizer();
+const tokenizer = new natural.RegexpTokenizer({ pattern: /[\s\.,!?\(\)]+/ });
 
 const router = express.Router();
 
@@ -24,7 +24,9 @@ router.post('/compare', (req, res) => {
     try {
         const quranText = "هُوَ اللَّهُ الَّذِي لَا إِلَٰهَ إِلَّا هُوَ ۖ الْمَلِكُ الْقُدُّوسُ السَّلَامُ الْمُؤْمِنُ الْمُهَيْمِنُ الْعَزِيزُ الْجَبَّارُ الْمُتَكَبِّرُ ۚ سُبْحَانَ اللَّهِ عَمَّا يُشْرِكُونَ";
         const userText = req.body.text; // النص الذي تم استقباله من العميل
+   
         const result = compareTexts(quranText, userText);
+
         res.send(result);
     } catch (error) {
         res.status(500).json({ error: error.message });
--- postHandler.js
+++ postHandler.js
@@ -2,8 +2,30 @@ import express from 'express';
 import fetch from 'node-fetch';
 import fs from 'fs';
 import upload from './multer.js'; // استيراد multerSetup
+import natural from 'natural';
 
+const tokenizer = new natural.RegexpTokenizer({ pattern: /[\s\.,!?\(\)]+/ });
 const router = express.Router();
+
+async function compareTexts(userText,quranText) {
+    try {
+       
+        const quranTokens = tokenizer.tokenize(quranText);
+        const userTokens = tokenizer.tokenize(userText);
+
+        let result = '';
+
+        quranTokens.forEach(quranToken => {
+            if (!userTokens.includes(quranToken)) {
+                result += quranToken + ' ';
+            }
+        });
+
+        return result.trim();
+    } catch (error) {
+        throw error;
+    }
+}
 
 router.post('/process-audio', upload.single('audioFile'), async (req, res) => {
     try {
@@ -13,21 +35,31 @@ router.post('/process-audio', upload.single('audioFile'), async (req, res) => {
             "https://api-inference.huggingface.co/models/tarteel-ai/whisper-base-ar-quran",
             {
                 headers: { Authorization: "Bearer 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" },
-                
                 method: "POST",
                 body: data,
             }
         );
         const result = await response.json();
+        
+        // استخراج النص من النتيجة المرجعية للمعالجة الصوتية
+        const userText = result.text;
+        console.log(userText)
+        // مقارنة النص مع النص القرآني
+        const quranText = req.body.quarntext;
+        const comparisonResult = await compareTexts(userText,quranText);
 
-        console.log(result.text);
-        res.json(result);
+        res.json({ original_text: userText, comparison_result: comparisonResult });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
 export default router;
+
+
+
+
+
 
 
 
